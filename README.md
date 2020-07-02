25b8093d29b18c5d672a26f209e8d092e4ae560b
Lucas C <luccoelhoo@gmail.com>
2020-07-02 04:25:50 -0300

    Upload de arquivos funcionais.

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-# print_pdf_all_codes_in_directory
+# print_all_codes_in_directory
 
-- Objetivo: Script que lê de forma recursiva todos os arquivos dentro de uma pasta (inclusive suas subpastas), independente do formato, junta todos os textos e códigos e salva em formato ".pdf" e ".txt". Indica, no texto compilado, o início e fim de cada arquivo e o caminho relativo de cada arquivo em relação à pasta passada como argumento.
+- Objetivo: Script que lê de forma recursiva todos os arquivos dentro de uma pasta (inclusive suas subpastas), independente do formato, junta todos os textos e códigos e salva em formato ".txt". Indica, no texto compilado, o início e fim de cada arquivo e o caminho relativo de cada arquivo em relação à pasta passada como argumento.
 
 - Usos: 
 	- \> python print_codes.py <nome_arquivo_saída> <caminho_pasta_alvo_do_processamento>
 
-- Saída: Dois arquivos, um em formato ".pdf" e outro em formato ".txt" na pasta em que o script estiver rodando.
+- Saída: Um arquivo, um em formato ".txt" na pasta em que o script estiver rodando.
 
 - Informações adicionais: 
 	
-	- Caso deseje mudar a fonte, a quantidade de caracteres na divisão dos arquivos, estilos, etc, edite a parte indicada no código.
 	- Esse script pode ser útil para produzir relatórios, pedidos de patente e de registro de software
+
+- Observações:
+	
+	- O programa dará erro para arquivos de mídia e imagens. Os seguintes formatos já estão filtrados:
+		- PNG, JPEG, JPG E ICO
+	- Se houver outros formatos gerando erro na saída do programa, é necessário adicionar no "if" dentro da função.
+	- Você pode usar o arquivo em .txt e abri-lo no Notepad++ ou outro editor para salvar como pdf. Não vale a pena uma aplicação em python que salve em pdf pelo fato de haver caracteres que não são triviais de converter, o índice de erros é muito altos.
--- print_codes.py
+++ print_codes.py
@@ -1,15 +1,17 @@
 from pathlib import Path
-from fpdf import FPDF
 import sys
 import os
-pdf = FPDF()
+#from fpdf import FPDF
+#pdf = FPDF()
 
 #################### IF NEEDED, MODIFY HERE ###################################################################
 
-pdf.set_font("Arial", size=10)
-alignment="L"
-width_cells=200
-height_cells=10
+#pdf.set_font("Arial", size=10)
+#alignment="L"
+#width_cells=200
+#height_cells=10
+
+
 string_separator="##########################################################################"
 
 ###############################################################################################################
@@ -23,7 +25,18 @@ def get_all_codes_from_direc(path_aux):
     for input_file in os.scandir(path_aux):
         text_aux=""
         #print(input_file)
-        if(not(input_file.name.endswith("png")) and not(input_file.name.endswith("jpeg")) and not(input_file.name.endswith("jpg"))   ):
+        if(
+            not(input_file.name.endswith("png"))
+            and not(input_file.name.endswith("jpeg"))
+            and not(input_file.name.endswith("jpg"))
+            and not(input_file.name.endswith("ico"))
+            and not(input_file.name.endswith("tiff"))
+            and not(input_file.name.endswith("ttf"))
+            and not(input_file.name.endswith("woff"))
+            and not(input_file.name.endswith("woff2"))
+            and not(input_file.name.endswith("eot"))
+            
+            ):
             if (input_file.is_dir()):
                 text+=get_all_codes_from_direc(input_file)
             else:
@@ -35,8 +48,8 @@ def get_all_codes_from_direc(path_aux):
                 text_aux+=str("caminho: "+(str(input_file.path)[(str(input_file.path).find(path)+len(path)):])+str("\n"))
                 text_aux+=string_separator
                 text_aux+="\n"
-                f=open(input_file,  'r')
-                text_aux+=f.read()
+                f=open(input_file,  'r', encoding='utf-8')
+                text_aux+=f.read().encode().decode('utf-8', 'replace')
                 f.close()
                 text_aux+="\n\n\n"
                 text_aux+=string_separator
@@ -51,14 +64,11 @@ name_file=sys.argv[1]
 path=sys.argv[2]
 text=""
 text=get_all_codes_from_direc(path)
-f=open(name_file+str(".txt"),"w")
+f=open(name_file+str(".txt"),"w",encoding='utf-8')
 f.write(text)
 f.close()
-pdf.add_page()
-pdf.multi_cell(width_cells, height_cells, txt=str(text), align=alignment)
-pdf.output(name_file+str(".pdf"))
+#pdf.add_page()
+#pdf.multi_cell(width_cells, height_cells, txt=str(text), align=alignment)
+#pdf.output(name_file+str(".pdf"))
 
 print("FINISHED, VERIFY ",Path.cwd())
-
-
-
